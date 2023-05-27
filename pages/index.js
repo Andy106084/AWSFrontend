793588d7b7a1839c729cd67c83dfd7b59c52e8f0
Andy106084 <76254396+Andy106084@users.noreply.github.com>
2023-05-27 15:52:41 +0800

    clean ssh (#25)
    * clean ssh
    
    * fix: prettier

--- pages/index.js
+++ pages/index.js
@@ -16,34 +16,34 @@ const tasks = [
 
 export default function Home() {
   // Calls Metamask to connect wallet on clicking Connect Wallet button
-    const [correctNetwork, setCorrectNetwork] = useState(false);
-    const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
-    const [, setCurrenAccount] = useState(false);
-    const [input, setInput] = useState("");
-    const [tasks, setTasks] = useState([]);
+  const [correctNetwork, setCorrectNetwork] = useState(false);
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [, setCurrenAccount] = useState(false);
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-    useEffect(() => {
-      // console.log("TaskAbit:", TaskAbiT);
-      // console.log("Abi:", TaskAbi.abi);
-      // connectWallet();
-      getAllTasks();
-    }, []);
+  useEffect(() => {
+    // console.log("TaskAbit:", TaskAbiT);
+    // console.log("Abi:", TaskAbi.abi);
+    // connectWallet();
+    getAllTasks();
+  }, []);
 
-    const connectWallet = async () => {
-      const { ethereum } = window;
-      if (ethereum) console.log("inside");
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      await provider.send("eth_requestAccounts", []);
-      const signer = provider.getSigner();
-      let address = await signer.getAddress();
-      setCurrenAccount(address);
-      setIsUserLoggedIn(true);
-      setCorrectNetwork(true);
-      console.log(address);
-    };
+  const connectWallet = async () => {
+    const { ethereum } = window;
+    if (ethereum) console.log("inside");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    let address = await signer.getAddress();
+    setCurrenAccount(address);
+    setIsUserLoggedIn(true);
+    setCorrectNetwork(true);
+    console.log(address);
+  };
 
   // Just gets all the tasks from the contract
-    const getAllTasks = async () => {
+  const getAllTasks = async () => {
     try {
       const { ethereum } = window;
       if (ethereum) {
@@ -61,7 +61,7 @@ export default function Home() {
 
   // Add tasks from front-end onto the blockchain
   const addTask = async e => {
-      e.preventDefault();
+    e.preventDefault();
 
     let task = {
       taskText: input,
